e60b1d21c00734667394f50a4bb56199d64a9ce7
chitranshsinghapsb-debug <chitranshsinghapsb@gmail.com>
2026-03-12 18:47:55 +0530

    Add files via upload

---
+++ day1_Bandname_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+name_of_the_city = input("What's the name of the city that they grew up in?\n")
+name_of_pet = input("What's your pet's name?\n")
+print("Your Band Name could be " + name_of_the_city +" ")
